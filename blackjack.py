1fe31279d4fb92aebf9b2f554afe2aa629e130eb
AnAde <82526914+AdeTheBoy@users.noreply.github.com>
2022-05-23 19:14:59 -0230

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -18,6 +18,10 @@ def check_score(cards):
         cards.remove(11)
         cards.append(1)
         return sum(cards)
+    elif sum(cards) == 21:
+        return 0
+    else:
+        return sum(cards)
 
 def compare(user_score, comp_score):
     if user_score > 21 and comp_score > 21:
@@ -32,6 +36,8 @@ def compare(user_score, comp_score):
         return "You went Over, you lose!"
     elif comp_score > 21:
         return "Opponent went Over, you win!"  
+    elif user_score > comp_score:
+        return "You win!"
     else:
         return "You lose"
 
@@ -42,8 +48,11 @@ def hit(cards):
         cards.append(draw_one())
         check_score(cards)
         print(f"Your cards are: {cards}")
-        print(f"Your total is: {cards}")
-        hit(cards)
+        print(f"Your total is: {sum(cards)}")
+        if sum(cards) == 21 or sum(cards) > 21:
+            return
+        else:
+            hit(cards)
     else:
         return 
 
@@ -55,26 +64,34 @@ def play_game():
     print("Welcome to blackjack by Ade :)")
     your_cards = []
     comp_cards = [] 
-    game_over  = False
     for _ in range(2):
         your_cards.append(draw_one())
         comp_cards.append(draw_one())
     print(f"Your cards are: {your_cards}")
+    print(f"Your total is: {sum(your_cards)}")
     print(f"The dealer's first card is {comp_cards[0]}")
-    while not game_over:
-        check_score(your_cards)
-        
-        game_over = True
+    total = sum(your_cards)
+    if total == 21 or total > 21:
+        ()
+    else:
+        hit(your_cards)
+    while sum(comp_cards) < 17 :
+        comp_cards.append(draw_one())
+    user_score = check_score(your_cards)
+    comp_score = check_score(comp_cards)
+    print(f"Dealer's cards are: {comp_cards}")
+    print(f"Dealer's total is: {sum(comp_cards)}")
+    print(compare(user_score,comp_score))
 
-
+game_stop = False
 """This asks a player if they want to start a
 new game of blackjack "y" == yes and "n" == no """
-while not game_over:
+while not game_stop:
     start = input("Would you like to play a game of blackjack? y/n: ")
     if start == "y":
         clear()
         play_game()
-        game_over = False
+        game_stop = False
     else:
         print("Goodbye!")
-        game_over = True
+        game_stop = True
