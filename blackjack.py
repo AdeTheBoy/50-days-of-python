e2662072c244dc823c4af855c737a245995aeabc
AnAde <82526914+AdeTheBoy@users.noreply.github.com>
2022-05-23 01:18:29 -0230

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -2,18 +2,6 @@ from art import logo
 from replit import clear
 import random
 
-"""This function asks a player if they want to start a
-new game of blackjack "y" == yes and "n" == no """
-def game_start():
-  start = input("Would you like to play a game of blackjack? y/n: ")
-  
-  if start == "y":
-      clear()
-      return False
-  else:
-      print("Goodbye!")
-      return True
-      
 """This function returns a list containing a random 
 card from a deck of cards"""
 def draw_one():
@@ -21,33 +9,72 @@ def draw_one():
     selection = random.choice(deck)
     return selection
 
-"""This function returns the sum of cards, and returns
+"""This function returns the sum of cards, or returns
 0 if the sum is equal to 21"""
-def calc_score(cards):
-    if sum(cards) == 21 & len(cards) == 2:
+def check_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
         return 0
-    else:
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
         return sum(cards)
 
-"""This function prints the final score and returns True
-in order to end the game"""
-def final():
-    print("You win")
-    return True 
+def compare(user_score, comp_score):
+    if user_score > 21 and comp_score > 21:
+        return "You went Over, you lose!"
+    if user_score == comp_score:
+        return "Draw!"
+    elif comp_score == 0:
+        return "Computer wins with a Blackjack"
+    elif user_score == 0:
+        return "You win with a Blackjack"
+    elif user_score > 21:
+        return "You went Over, you lose!"
+    elif comp_score > 21:
+        return "Opponent went Over, you win!"  
+    else:
+        return "You lose"
 
-game_over = game_start()
+"""This function asks the player if they would like another card, and deals one if they want one"""
+def hit(cards):
+    one_more = input("Would you like another card? y/n: ")
+    if one_more == "y":
+        cards.append(draw_one())
+        check_score(cards)
+        print(f"Your cards are: {cards}")
+        print(f"Your total is: {cards}")
+        hit(cards)
+    else:
+        return 
 
-while not game_over:
+def final(your_cards, comp_cards):
+    print()
+
+def play_game():
     print(logo)
     print("Welcome to blackjack by Ade :)")
     your_cards = []
-    comp_cards = []
+    comp_cards = [] 
+    game_over  = False
     for _ in range(2):
         your_cards.append(draw_one())
         comp_cards.append(draw_one())
     print(f"Your cards are: {your_cards}")
     print(f"The dealer's first card is {comp_cards[0]}")
-    if sum(your_cards) == 21 & len(your_cards) == 2:
-        final()
-    game_over = final()
- 
+    while not game_over:
+        check_score(your_cards)
+        
+        game_over = True
+
+
+"""This asks a player if they want to start a
+new game of blackjack "y" == yes and "n" == no """
+while not game_over:
+    start = input("Would you like to play a game of blackjack? y/n: ")
+    if start == "y":
+        clear()
+        play_game()
+        game_over = False
+    else:
+        print("Goodbye!")
+        game_over = True
