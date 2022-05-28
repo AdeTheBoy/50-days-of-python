acb2de36516d2f07ce8d053e5728cc377581e26c
AnAde <82526914+AdeTheBoy@users.noreply.github.com>
2022-05-27 22:24:57 -0230

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,12 +1,3 @@
-#Number Guessing Game Objectives:
-
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 import random
 from art import logo
 
@@ -38,6 +29,20 @@ Psst the correct answer is {rand_number}"""
     )
     no_of_lives = calc_lives()
     print(f"You have {no_of_lives} attempts to guess the number.")
-    
+    while no_of_lives > 0:
+        guess = int(input("Guess a number: "))
+        if guess > rand_number:
+            no_of_lives -= 1
+            print(f"Too high!\nYou have {no_of_lives} left.")
+        elif guess < rand_number:
+            no_of_lives -= 1
+            print(f"Too Low!\nYou have {no_of_lives} left.")
+        elif guess == rand_number:
+            no_of_lives -= no_of_lives
+
+    if guess == rand_number:
+        print("You Win!")
+    else:
+        print("You Lose :(")
     game_end = True
     
